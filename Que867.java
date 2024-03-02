17013bf3b1533a2af0c381b20fc426df7545eeb7
satish998 <satishkumar04121998@gmail.com>
2024-03-02 13:05:17 +0530

    new file added

---
+++ Que867.java
@@ -0,0 +1,13 @@
+// 867. Transpose Matrix
+class Solution {
+  public int[][] transpose(int[][] matrix) {
+      int newarr[][]=new int[matrix[0].length][matrix.length];
+      for(int i=0;i<matrix.length;i++){
+          for(int j=0;j<matrix[0].length;j++){
+              newarr[j][i]= matrix[i][j];
+          }
+      }
+      return newarr;
+      
+  }
+}
